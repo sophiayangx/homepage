b67d175fc61907780d81612a2164dc8a67e71dbe
sophiayangx <sopyang@nuevaschool.org>
2016-12-20 20:28:40 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -44,3 +44,20 @@ var colors = ["#e57b7b", "#efbe9b", "#f9fcb8", "#bdfcb8", "#91add6", "#aa91d6", 
 colors.forEach(function (color) {
   $(elementsList).append(element.clone().css("background-color", color))
 });
+
+//hack the editor
+var button = $("#hackbutton")
+
+button.click(function(){
+  var webpage = $(parent.document.body)
+  webpage.find("div").css({border:"1px solid yellow"})
+
+  var block = $(parent.parent.document.body).find("#b5")
+  block.find("pre").append("<button class='btn btn-primary' id='hi'>" + "hi" + "</button>");
+});
+
+$("#hackbutton").click(function(e){
+  e.preventDefault();
+});
+
+//politics
