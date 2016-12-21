0c835a0bb5e9804ba8b9e1ea8ee1f60d0c910628
sophiayangx <sopyang@nuevaschool.org>
2016-12-20 17:27:34 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-var commentInput = $("#comment-input")
-var commentDisplay = $(".comment-display")
+$('#button').click(function(){
+  $('html,body').animate({
+    scrollTop: $('.wrapper-projtitle').offset().top},
+                         'slow');
+});
 
-commentInput.keydown(function(e) {
-  if (e.which == 13) {
-    addListItem("comments", commentInput.val());
+$('#carrot').click(function(){
+  $('html,body').animate({
+    scrollTop: $('.wrapper-projects').offset().top},
+                         'slow');
+});
+
+//text effects
+var shown = true
+setInterval(function() {
+  if (shown) {
+    $("#blinking").css({"visibility": "hidden"});
+    shown = false;
+  } else {
+    $("#blinking").css({"visibility": "visible"});
+    shown = true;
   }
-})
+}, 800)
 
-onNewListItem("comments", function(newComment) {
-  commentDisplay.append("<div>" + newComment + "</div>")
-})
+//deal with it
+$('#animateblock').hover(function() {
+  $(this).animate({
+    left: '195px'
+  });
+});
+
+$('#animateblock').mouseleave(function() {
+  $(this).animate({
+    left: '15px'
+  });
+});
